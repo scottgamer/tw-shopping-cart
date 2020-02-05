86920bb0749ed1a658b50c04bfeb3d1374ee73bb
richardmunoz <richard.munoz@crifa.com>
2020-02-05 17:02:21 -0500

    added test for offer 2 products only pay 1

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -2,7 +2,7 @@ const ShoppingCart = require('../src/ShoppingCart.js');
 
 describe('should run the app', () => {
   it('gets the price right with DIS_10', () => {
-    const item = { name: 'foo', price: '10', productCode: 'DIS_10' };
+    const item = { name: 'foo', price: 10, productCode: 'DIS_10' };
     const shoppingCart = new ShoppingCart([item]);
     const results = shoppingCart.checkout();
     expect(results.totalPrice).toEqual(9);
@@ -10,7 +10,7 @@ describe('should run the app', () => {
 
   it('should return all products', () => {
     const products = require('../src/data/products.json');
-    expect(products.length).toEqual(10);
+    expect(products.length).toEqual(11);
     expect(products[0].productCode).toEqual('CHAIR_RED');
     expect(products[1].productCode).toEqual('DIS_10-CHAIR_BLUE');
   });
@@ -20,5 +20,14 @@ describe('should run the app', () => {
     const shoppingCart = new ShoppingCart([item]);
     const results = shoppingCart.checkout();
     expect(results.totalPrice).toEqual(16);
+  });
+
+  it('should return price of only one product with code BULK_BUY_2_GET_1', ()=>{
+    const item1 = { name: 'foo', price: 20, productCode: 'BULK_BUY_2_GET_1' };
+    const item2 = { name: 'bar', price: 20, productCode: 'BULK_BUY_2_GET_1' };
+
+    const shoppingCart = new ShoppingCart([item1, item2]);
+    const results = shoppingCart.checkout();
+    expect(results.totalPrice).toEqual(20);
   });
 });
