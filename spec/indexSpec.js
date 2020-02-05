87e8a02df45fb40423776298bb93edd28ce1b5d7
richardmunoz <richard.munoz@crifa.com>
2020-02-05 15:48:21 -0500

    added unit test for DISC20

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,19 +1,24 @@
-var ShoppingCart = require('../src/ShoppingCart.js')
+const ShoppingCart = require('../src/ShoppingCart.js');
 
-describe('should run the app', function() {
-
-  it('gets the price right with DIS_10', function() {
-    var item = {name: "foo", price: "10", productCode: "DIS_10"}
-    var shoppingCart = new ShoppingCart([item]);
-    var results = shoppingCart.checkout();
+describe('should run the app', () => {
+  it('gets the price right with DIS_10', () => {
+    const item = { name: 'foo', price: '10', productCode: 'DIS_10' };
+    const shoppingCart = new ShoppingCart([item]);
+    const results = shoppingCart.checkout();
     expect(results.totalPrice).toEqual(9);
   });
 
-  xit('should return all products', function() {
-    const products = require('../src/data/products.json')
-    expect(products).toEqual(2);
-    expect(products[0].productCode).toEqual("CHAIR_RED");
-    expect(products[1].productCode).toEqual("DIS_10-CHAIR_BLUE");
+  it('should return all products', () => {
+    const products = require('../src/data/products.json');
+    expect(products.length).toEqual(10);
+    expect(products[0].productCode).toEqual('CHAIR_RED');
+    expect(products[1].productCode).toEqual('DIS_10-CHAIR_BLUE');
   });
 
+  it('should get the price right with DIS_20', ()=>{
+    const item = { name: 'foo', price: '20', productCode: 'DIS_20' };
+    const shoppingCart = new ShoppingCart([item]);
+    const results = shoppingCart.checkout();
+    expect(results.totalPrice).toEqual(16);
+  });
 });
